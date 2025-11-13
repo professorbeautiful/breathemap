b093ed311a45998f91b69a6ffe03f58170c087e8
Roger Day <notsofeo@gmail.com>
2025-11-13 16:43:31 -0500

    averaging the 2 lats and lons, might eliminate the occasional "oops, the town next door" error.

--- appPA/server.R
+++ appPA/server.R
@@ -3,17 +3,35 @@ function(input, output, session) {
   load('tracts_with_towns.Rd')  ### shoudl be in the folder appPA
   tracts = PAtowndata$NAMELSAD
   towns = tracts_with_towns$towns[match(tracts, tracts_with_towns$tracts)]
+
   towns [ is.na(towns )] = '___'
-  lats = tracts_with_towns$lat.x[match(tracts, tracts_with_towns$tracts)]
-  lons = tracts_with_towns$lon.x[match(tracts, tracts_with_towns$tracts)]
-  PAtownnames = PAtowndata$Town = paste(towns, tracts, sep= ', ')
+  tracts_with_towns$towns [ is.na(tracts_with_towns$towns )] = '___'
+
+  lats.x = as.numeric(tracts_with_towns$lat.x[match(tracts, tracts_with_towns$tracts)])
+  lons.x = as.numeric(tracts_with_towns$lon.x[match(tracts, tracts_with_towns$tracts)])
+  lats.y = as.numeric(tracts_with_towns$lat.y[match(tracts, tracts_with_towns$tracts)])
+  lons.y = as.numeric(tracts_with_towns$lon.y[match(tracts, tracts_with_towns$tracts)])
+  #plot(lats.x, lats.y); plot(lons.x, lons.y);
+  lats = (lats.x+lats.y)/2    ### slightly more accurate, probably
+  lons = (lons.x+lons.y)/2
+  PAtownnames = paste(towns, tracts, sep= ', ')
   PAtown$TOWN = PAtown$NAME = PAtownnames[match(tracts, PAtown$NAMELSAD)]
   PAtowndata$TOWN =PAtowndata$NAME = PAtownnames[match(tracts, PAtowndata$NAMELSAD)]
   PAtowndata$lat = lats[match(tracts, PAtowndata$NAMELSAD)]
   PAtowndata$lon = lons[match(tracts, PAtowndata$NAMELSAD)]
+
   PAtown$lat = lats[match(tracts, PAtown$NAMELSAD)]
   PAtown$lon = lons[match(tracts, PAtown$NAMELSAD)]
-  #  these lat and lon do locate correctly.
+  #  these lat and lon do seem to locate correctly.  checking against https://data.jsonline.com/census/total-population/ and US census.
+  # But needs more checking.
+
+
+  # sort everything by
+  townOrder = order(towns, na.last=TRUE)
+  tracts_with_towns = tracts_with_towns[townOrder, ]
+  towns = towns[townOrder]
+  # PAtown = PAtown[townOrder, ]
+  # PAtowndata = PAtowndata[townOrder, ]
 
 
 
