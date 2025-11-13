88bc2899b9b330f7fd4b9ca675f5f8edadb1b303
Roger Day <notsofeo@gmail.com>
2025-11-13 13:11:40 -0500

    runApp for either state.

--- run.R
+++ run.R
@@ -2,13 +2,19 @@ library(shiny)
 State = 'PA'
 wd = getwd()
 appname = paste0('app', State)
-if( ! (basename(wd) == appname) )
+if( ! (basename(wd) == appname) ){
+  if(file.exists(appname))
     setwd(appname)
+  else if(file.exists(paste0('../', appname)))
+    setwd(paste0('../', appname))
+
+}
 # setwd("C:\\Users\\lukem\\Desktop\\BC_developer\\0224\\0308\\0625_FINAL\\app")
 runApp()
-setwd(wd)
+
+setwd('..')
 # sometimes setwd() is funky when running R locally. and setwd(".") won't work
-# if this is the case, use setwd("~localpath") for development, but comment it
+# if this is the case, use set awd("~localpath") for development, but comment it
 # out when uploading to actual server
 
 # required packages:  sf  leaflet DT data.table ggplot2
