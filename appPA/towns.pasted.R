51435a3ed3b90e441958ad5284c70751d48bd3a1
Roger Day <notsofeo@gmail.com>
2025-12-28 11:19:49 -0500

    Argghh  . Correcting error where NA became "NA".

--- appPA/towns.pasted.R
+++ appPA/towns.pasted.R
@@ -3,10 +3,11 @@ towns.pasted =  function(D){
     temp = D %>% dplyr::group_by(tracts)
   #temp %>% summarise(townsAll=paste0(towns, collapse=','))
   temp = split(D, D$tracts)
-  temp.t = lapply(temp, function(t) paste(t$towns, collapse=','))
+  temp.t = sapply(temp, function(t) paste(t$towns, collapse=','))
   temp.3 = data.frame(tracts = unique(D$tracts))
   temp.3$towns = temp.t[match(temp.3$tracts, names(temp.t))]
-  dim(temp.3)  #108
+  temp.3$towns[temp.3$towns == "NA"] = NA
+  dim(temp.3)
   return(temp.3)
 }
 
