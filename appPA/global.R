9022e7616a0f02e37eabb343e1745769c895aa5b
Roger Day <notsofeo@gmail.com>
2025-12-28 14:49:26 -0500

    Trying to do away with PAtown and PAtowndata. At least the map *almost* shows up.

--- appPA/global.R
+++ appPA/global.R
@@ -15,9 +15,15 @@ library(shinyDebuggingPanel)
 # loads data required for app
 
 
-load('tracts_with_towns.Rd')   ### tt6.sw.l southwest PA with Lemery
-load('PAtown.Rd')   ## 26 fields only
-load('PAtowndata.Rd' )
+load('tracts_with_towns.Rd')
+load('patown1.Rd')   ## 26 fields only
+PAtown = patown1
+load('patowndata2.Rd' )   # same as
+load('PAtowndata.lukedata.Rd')
+PAtowndata = PAtowndata.lukedata
+PAtowndata$`All-cause deaths` = rowMeans(PAtowndata[c(
+  'All Cause Deaths, Laden Estimate',
+  'All Cause Deaths, Krewski Estimate') ])
 
 st_crs(PAtown) <- "WGS84"   ### no effect on the app apparently.
 ### removes the error msg,
@@ -26,35 +32,28 @@ st_crs(PAtown) <- "WGS84"   ### no effect on the app apparently.
 ### But gives us a new  error msg,
 ###Warning: st_crs<- : replacing crs does not reproject data; use st_transform for that
 
-load('PAtowndata.lukedata.Rd')
-PAtowndata = PAtowndata.lukedata
-PAtowndata$`All-cause deaths` = rowMeans(PAtowndata[c(
-  'All Cause Deaths, Laden Estimate',
-  'All Cause Deaths, Krewski Estimate') ])
-load('tracts_with_towns.Rd')  ### should be in the folder appPA
 
-tracts = PAtowndata$NAMELSAD
-towns = tracts_with_towns$towns[match(tracts, tracts_with_towns$tracts)]
+#  tracts_with_towns = tt1.sw # from geodataPA
+
+# tracts = PAtowndata$NAMELSAD
+# towns = tracts_with_towns$towns[match(tracts, tracts_with_towns$tracts)]
+towns = tracts_with_towns$towns
 townOrder = order(towns, na.last=TRUE)
 
+####   15 are missing still.
 towns [ is.na(towns )] = '___'
 tracts_with_towns$towns [ is.na(tracts_with_towns$towns )] = '___'
-
-lats.x = as.numeric(tracts_with_towns$lat.x[match(tracts, tracts_with_towns$tracts)])
-lons.x = as.numeric(tracts_with_towns$lon.x[match(tracts, tracts_with_towns$tracts)])
-lats.y = as.numeric(tracts_with_towns$lat.y[match(tracts, tracts_with_towns$tracts)])
-lons.y = as.numeric(tracts_with_towns$lon.y[match(tracts, tracts_with_towns$tracts)])
-#plot(lats.x, lats.y); plot(lons.x, lons.y);
-lats = (lats.x+lats.y)/2    ### slightly more accurate, probably
-lons = (lons.x+lons.y)/2
-PAtownnames = paste(towns, tracts, sep= ', ')
-PAtown$TOWN = PAtown$NAME = PAtownnames[match(tracts, PAtown$NAMELSAD)]
-PAtowndata$TOWN =PAtowndata$NAME = PAtownnames[match(tracts, PAtowndata$NAMELSAD)]
-PAtowndata$lat = lats[match(tracts, PAtowndata$NAMELSAD)]
-PAtowndata$lon = lons[match(tracts, PAtowndata$NAMELSAD)]
-
-PAtown$lat = lats[match(tracts, PAtown$NAMELSAD)]
-PAtown$lon = lons[match(tracts, PAtown$NAMELSAD)]
+townIs___ =  (towns == '___')
+tracts_with_towns$lat.places[townIs___ ] =  tracts_with_towns$lat.tracts[townIs___ ]
+tracts_with_towns$lon.places[townIs___ ] =  tracts_with_towns$lon.tracts[townIs___ ]
+# PAtownnames = paste(towns, tracts, sep= ', ')
+# PAtown$TOWN = PAtown$NAME = PAtownnames[match(tracts, PAtown$NAMELSAD)]
+# PAtowndata$TOWN =PAtowndata$NAME = PAtownnames[match(tracts, PAtowndata$NAMELSAD)]
+# PAtowndata$lat = lats[match(tracts, PAtowndata$NAMELSAD)]
+# PAtowndata$lon = lons[match(tracts, PAtowndata$NAMELSAD)]
+#
+# PAtown$lat = lats[match(tracts, PAtown$NAMELSAD)]
+# PAtown$lon = lons[match(tracts, PAtown$NAMELSAD)]
 #  these lat and lon do seem to locate correctly.  checking against https://data.jsonline.com/census/total-population/ and US census.
 # But needs more checking.
 
@@ -62,21 +61,46 @@ PAtown$lon = lons[match(tracts, PAtown$NAMELSAD)]
 # sort everything by
 tracts_with_towns = tracts_with_towns[townOrder, ]
 towns = towns[townOrder]
-PAtown = PAtown[townOrder, ]
-PAtowndata = PAtowndata[townOrder, ]
-PAtownnames = PAtownnames[townOrder]
+names(PAtowndata)
+featureList= c("Myocardial Infarctions", "COPD Deaths", "Ischemic Heart Disease Deaths",
+               # "All Cause Deaths, Laden Estimate"  ,
+               # "All Cause Deaths, Krewski Estimate", "All Cause Deaths, Lepeule Estimate",
+               # "All Cause Deaths, Di Estimate",
+               "All-cause deaths", # (avg Krewski, Laden)
+               "Low Birth Weight Babies", "Preterm Births", "Stillbirths",
+               "Total Population (2019)", "PM2.5 average")
 
-PAtown$townName = gsub(', .*Census Tract.*', '', PAtowndata$TOWN )
-PAtown$towntractName = PAtowndata$TOWN
-PAtown$noTown = PAtown$townName == '___'
-table (PAtown$noTown)
-PAtown$areaField = PAtown$towntractName  ### until toggled
-PAtownExtra = setdiff(y=names(PAtown), names(PAtowndata))
-PAtown[PAtownExtra] = PAtowndata[PAtownExtra]
-class(PAtown)
-names(PAtown)
+#### Bring in featureList.  Careful PM_avg
+PAtowndata$tracts = PAtowndata$GEOID
+PAtowndata$`PM2.5 average` = PAtowndata$PM_avg
+tracts_with_towns.wide = merge(tracts_with_towns, PAtowndata[c('tracts', 'Tract Name', featureList)])
+names(tracts_with_towns.wide) = gsub(' ', '__', names(tracts_with_towns.wide))
+tracts_with_towns.wide = (tracts_with_towns.wide %>%
+                            select(c(tracts:towns.tt1, Tract__Name:geometry)))
+names(tracts_with_towns.wide)
+names(tracts_with_towns.wide) = gsub('__', ' ', names(tracts_with_towns.wide))
+names(tracts_with_towns.wide)
+
+tracts_with_towns.wide$areaField = tracts_with_towns.wide$towns
+### for other fields to use for label, we will copy onto areaField.
+
+
+# PAtown$townName = gsub(', .*Census Tract.*', '', PAtowndata$TOWN )
+# PAtown$towntractName = PAtowndata$TOWN
+# PAtown$noTown = PAtown$townName == '___'
+# table (PAtown$noTown)
+
+# PAtown$townName = gsub(', .*Census Tract.*', '', PAtowndata$TOWN )
+# PAtown$towntractName = PAtowndata$TOWN
+# PAtown$noTown = PAtown$townName == '___'
+# table (PAtown$noTown)
+# PAtown$areaField = PAtown$towntractName  ### until toggled
+# PAtownExtra = setdiff(y=names(PAtown), names(PAtowndata))
+# PAtown[PAtownExtra] = PAtowndata[PAtownExtra]
+# class(PAtown)
+# names(PAtown)
 ##  OK, from here on, no more PAtowndata
-PAtown$`Total Population (2019)` = as.numeric(PAtown$`Total Population (2019)`)
+# PAtown$`Total Population (2019)` = as.numeric(PAtown$`Total Population (2019)`)
 
 # creates headers for the datatables. Referenced in server.R
 demogcaption <- htmltools::tags$caption(
@@ -95,10 +119,3 @@ IQcaption <- htmltools::tags$caption(
   style = 'font-weight: bold; text-align: center; color:#FFFFFF; background-color:#8a100b; padding:0.5em;',
   'Estimated IQ Loss')
 
-featureList= c("Myocardial Infarctions", "COPD Deaths", "Ischemic Heart Disease Deaths",
-               # "All Cause Deaths, Laden Estimate"  ,
-               # "All Cause Deaths, Krewski Estimate", "All Cause Deaths, Lepeule Estimate",
-               # "All Cause Deaths, Di Estimate",
-               "All-cause deaths", # (avg Krewski, Laden)
-               "Low Birth Weight Babies", "Preterm Births", "Stillbirths",
-               "Total Population (2019)", "PM_avg")
