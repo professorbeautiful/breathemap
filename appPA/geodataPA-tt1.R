5826a7ffa15a117cc3693ce56b96c21d04be75d8
Roger Day <notsofeo@gmail.com>
2025-12-28 12:58:33 -0500

    adding lat and lon from pa_places. Useful for showTowns leaflet

--- appPA/geodataPA-tt1.R
+++ appPA/geodataPA-tt1.R
@@ -74,9 +74,14 @@ table( is.na(tt1.sw$places), tt1.sw$onlyLuke, dnn = cq('placesNA luke'))
 #### TODO - ONLY if needed: latitudes and longitudes ####
 # Add latitudes and longitudes for Luke tracts
 # Add latitudes and longitudes for town from towns.intersects.first
-
-
+tt1.sw$lat.places.x = pa_places$lat.places[
+  match(tt1.sw$towns.intersects.first, pa_places$places)]
+tt1.sw$lon.places.x = pa_places$lon.places[
+  match(tt1.sw$towns.intersects.first, pa_places$places)]
+table(tt1.sw$lon.places.x == tt1.sw$lon.places)
+View(cbind(tt1.sw$lat.places.x, tt1.sw$lat.places) ) ## not total agreement, 244 yes, 152 no
 names(tt1.sw)
+
 tt1.sw.names = c("tracts", "towns", "towns.intersects"
                  , "towns.intersects.first", "places", "county.tracts",  "tracts.short",
                  "onlyLuke",
@@ -95,132 +100,13 @@ table(is.na(tt1.sw$towns))  ## 25 HOLES.
 
 ####   addLemeryPlaces  ####
 #  Pick up better names from Lemery
+sum(is.na(tt1.sw$towns))
 source('appPA/addLemeryPlaces.R', local = T)
-
-#### summarize tract_counts ####
-tract_counts = table(tt1.sw$tracts)
-tract_counts_table = table(tract_counts)
-sum(tract_counts_table)  # 808 now
-sum(tract_counts_table * as.numeric(names(tract_counts_table)))   ## 1942 -> 2082 now
-
-tt1.sw.save$tract_count = tract_counts[match(tt1.sw.save$tracts, names(tract_counts))]
-mostPlacesForATract = max(as.numeric(names(tract_counts_table)))   #13
-tt1.sw.save %>% subset(tract_count == mostPlacesForATract) %>% select(places)
-(tt1.sw.save %>% subset(tract_count == mostPlacesForATract) %>% select(tracts)  )$tracts[1]
-#  13 places for 42125784000.
-#  I checked these places at the DEHE map. Roughly corresponding, not perfect.
-
-#### these places have only one tract ####
-tt1.sw.save.1tract = tt1.sw.save[tt1.sw.save$tract_count == 1 , ]
-### 250,  the same as tract_counts_table[1]
-
-#### summarizeplace_counts ####
-place_counts = table(tt1.sw.save$places)
-place_counts_table = table(place_counts)
-sum(place_counts_table)  #398
-sum(place_counts_table * as.numeric(names(place_counts_table)))  #1911 -> 2051
-tt1.sw.save$place_count = place_counts[match(tt1.sw.save$places, names(place_counts))]
-dim(tt1.sw.save %>% subset(place_count == max(as.numeric(names(place_counts_table)))) %>%
-      select(places)
-  )   ### 208 rows now  Pittsburgh.  Was 180.  correct.
-
-
-### When there are multiple towns for a tract, pick the first one, or else paste. ####
-### When there are multiple tracts for a  town... not our problem. ####
-#### since our feature data is based on tracts, we need the tract geometry. ####
-
-#### Add tracts from PAtowndata.lukedata ####
-
-table(tt1.sw.save$tracts %in% PAtowndata.lukedata$GEOID) # 1937 was 1797
-table(unique(tt1.sw.save$tracts) %in% PAtowndata.lukedata$GEOID) # we have data for 739, was 683 tracts
-table(PAtowndata.lukedata$GEOID %in% unique(tt1.sw.save$tracts)) # also 739.
-## therefore, restrict tt1.sw to the 739 tracts.
-tt1.sw = tt1.sw.save[tt1.sw.save$tracts %in% PAtowndata.lukedata$GEOID, ]
-dim(tt1.sw)   ### 1937
-
-place_counts = table(tt1.sw$places, exclude=NULL)  ### still 25 missing places
-place_counts_table = table(place_counts, exclude=NULL)
-sum(place_counts_table)  #399
-sum(place_counts_table * as.numeric(names(place_counts_table)))  #1937, was 1772 + 25
-tt1.sw$place_count = place_counts[match(tt1.sw$places, names(place_counts))]
-# don't worry about tract count. tt1.sw$tract_count = tract_counts[match(tt1.sw$tracts, names(tract_counts))]
-tt1.sw %>% subset(tract_count ==13 ) %>% select(places)  ### still 13
-(tt1.sw %>% subset(tract_count ==13) %>% select(tracts)  ) [1,1] #42125784000
-
-# Check geometry:  Greensburg has 9 tracts.  Check the geom places vs tracts
-tt1.sw[1, ]
-st_area(print(tt1.sw[1, 'geometry']))  ### 1013591
-st_area(print(pa_places [ pa_places$places=='Greensburg', 'geometry'])) #10491674
-# so the area of Greensburg is bigger...  should the tracts add? This is about 1/10.
-1013591/10491674
-#  tracts could intersect multiple places, so we don't know for sure.
-sum(st_area(print(tt1.sw[tt1.sw$places=='Greensburg', 'geometry']))  ### 1013591
-) # 74843515.   No the tracts must overlap other places.  Probably ok.
-
-10491674/74843515  ## 14% all Greensburg still smaller than the union of all the tracts.
-
-tt_intersections = st_intersects(pa_tracts_sw$geometry, pa_tracts_sw$geometry)
-View(as.data.frame(tt_intersections))
-# (tt_intersections)   list 1752 × 7521 (S3: sgbp, list List of length 752
-
-tt1.sw.reverse = st_join(pa_places, pa_tracts_sw, left=TRUE) # 3401
-tt1.sw.reverse.tt = tt1.sw.reverse[c('places', 'tracts')]
-tt1.sw.tt = tt1.sw[c('places', 'tracts')]
-#do they at least agree on the 1 to 1 cases?  Probably not.
-
-
-
-#     st_contains
-# tt1 =  st_join(pa_tracts[ , c('lat', 'lon','latlon', 'tracts')],
-#                              pa_places[ , c('lat', 'lon','latlon', 'towns')],
-#                               join=st_intersects
-#  )
-### this was garbage.  Here' where phildelphia jumped in.
-### write out for input into the app.
-
-#### vastly faster computation than copilot's join.
-# plot(tracts_with_towns$lat.x, tracts_with_towns$lat.y)
-# plot(tracts_with_towns$lon.x, tracts_with_towns$lon.y)
-#### OK.  close, NOT exact.  No wonder copilot code broke.
-
-
-# pa_intersects_most = st_intersects_most(pa_tracts, pa_places)
-# Error: Can't coerce from an integer to a string.
-# Called from: flatten_chr(.)
-
-#3139855 Nov 13 17:23 PAtown.Rd
-dim(PAtown)   ## 739
-names(PAtown)
-
-#####################################################################
-# DEAD END.  crap from copilot.  SEE ABOVE
-# Spatial join: assign each tract to the town it intersects most with
-
-## this takes several minutes. NOT NEEDED
-
-# tt6.sw <-  st_join(pa_tracts_sw, pa_places_sw, join = st_intersects, largest = TRUE)
-# dim(tt6)
-# names(tt6)
-#tt6.allPA = tt6
-
-leaflet::leaflet() %>% addTiles() %>%
-  addPolygons(
-              data=tt1.sw,
-              label = ~tracts )
-
-PAtowndata.lukedata %>% filter(GEOID == '42003010300')  # it's there, but not in map
-pa_tracts_sw %>% filter(tracts == '42003010300')  ## here it is finally!
-tt1.sw %>% filter(tracts == '42003010300')  ## here it is finally!  towns = Pittsburgh
-tractsLemeryPgh %>% filter(tracts == '42003010300')  ## not here though. that's ok.
-###
-
-# ## RD code correction... copilot doesn't know about the .x .y copies maintained.
 
 #####  add Lemery information
 tt1.sw.l = tt1.sw
 dim(tt1.sw.l)
 
-tt1.sw.l$lem.towns = NA
 tt1.sw.l$lem.towns = tractsLemery$towns[
   match(tt1.sw.l$tracts, tractsLemery$tracts)]
 
