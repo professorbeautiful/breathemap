d9f937539bfdca3ade4439dc62d1bcc0613d34c6
Roger Day <notsofeo@gmail.com>
2025-12-28 12:54:33 -0500

    showTowns   first commit. showTowns = function(towns, tt=tt1.sw,                townsField = c('towns', 'towns.intersects', 'towns.intersects.first') 1], perfect=TRUE, zoom=9)

---
+++ appPA/leaflet-test.R
@@ -0,0 +1,35 @@
+# doesn't work.
+showTowns = function(towns, tt=tt1.sw,
+                     townsField = c('towns', 'towns.intersects', 'towns.intersects.first')
+                     [1], perfect=TRUE, zoom=9) {
+  if(is.numeric(towns))
+    tt = tt[towns, ]
+  if(is.numeric(townsField))
+    townsField = c('towns', 'towns.intersects', 'towns.intersects.first')[townsField]
+  tt$towns = tt[[townsField]]
+  if(is.character(towns) & !perfect)
+    towns = grep(towns, tt$towns, v=T)
+
+  tt = tt[tt$towns %in% towns, ]
+  lng = mean(as.numeric(tt$lon.places), na.rm=T)
+  lat = mean(as.numeric(tt$lat.places), na.rm=T)
+  leaflet() %>%
+  addProviderTiles("CartoDB.PositronNoLabels",
+                   options = tileOptions(minZoom = 5, maxZoom = 11)) %>%
+  setView(lng = lng, lat = lat, zoom=zoom)  %>%
+  addPolygons(data = tt,
+              weight = 1,
+              color = "Black",
+              fillColor = "blue",
+              fillOpacity = 0.3,
+              # label is the label shown
+              #label = ~areaField, #works ok. PAtown[['NAME']] = PAtown[['areaField']]
+              label = ~towns, #PAtown[['NAME']] = PAtown[['areaField']]
+              #layerId = ~towntractName, ## initially.
+              highlight = highlightOptions(
+                fillColor = "green",
+                color = "red",
+                weight = 2,
+                fillOpacity = 1,
+                bringToFront = T))
+}
