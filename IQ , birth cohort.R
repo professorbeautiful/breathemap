987ad1917b0b16ef7dde4755bab24683d3df2cc0
Roger Day <notsofeo@gmail.com>
2026-01-30 17:56:02 -0500

    Reproducing and extending my previous birth cohort analysis

--- IQ , birth cohort.R
+++ IQ , birth cohort.R
@@ -1,59 +1,98 @@
 #### IQ , birth cohort
 
 #' from ella: 2019 PA birth cohort_censustracts_county .xlsx'
+#' All births in PA in 2019
+#'
+#'
 
-cohort.pa = readr::read_csv('2019-counties-tracts - Pennsylvania_Birth_2019.csv.csv', col_types = 'cccc')
-#ok, noread.csv()#ok, now character.
-#### for searching, gsheet is better.
-dim(cohort.pa)
-head(cohort.pa)
-## argh, dropping leading zeros despite "c'.
-names(cohort.pa)
-#shd be 130532
+install.packages("googlesheets4")
+library("googlesheets4")
+
+# reading the sheet data
+sheet_data <-read_sheet(    ##### doesn't work... Client error: (400) FAILED_PRECONDITION?
+  'https://docs.google.com/spreadsheets/d/1NGwXkJ2TH_sP_O1dwT2Hx1jrawxGWhL0/edit?gid=1240450136#gid=1240450136'
+)
+
+
+ellaAllBirths.xl = readxl::read_excel(
+  '2019 PA birth cohort_censustracts_county .xlsx')
+###  identical read either way
+ellaAllBirths = read.csv(
+  '2019 PA birth cohort_censustracts_county .xlsx - Pennsylvania_Birth_2019.csv.csv', )
+#' Tract needs to  be padded.
+#'
+ellaAllBirths$tract = 1e10 + ellaAllBirths$Tract
+
+head(ellaAllBirths)
+ellaAllBirths$tract = as.character(ellaAllBirths$tract)
+head(ellaAllBirths)
+ellaAllBirths$tract = substr(ellaAllBirths$tract, 6,13)
 source('appPA/countymap.R')
-cohort.pa[3089,] #  030500  next one empty
-cohort.pa[3090,] #    this one empty
-cohort.pa.sw = cohort.pa[cohort.pa$County %in% countymap$county, ]  ##ok.
-cohort.pa.sw$countynum = countymap$COUNTYFP[match(cohort.pa.sw$County,countymap$county)]
-table(cohort.pa.sw$County, cohort.pa.sw$countynum)
-cohort.by.tract = paste0(cohort.pa.sw$countynum, cohort.pa.sw$Tract)
-cohort.by.tract = table(cohort.by.tract)
-cohort.by.tract = data.frame(tract = names(cohort.by.tract), births = as.vector(cohort.by.tract))
-head(cohort.by.tract)
-dim(cohort.by.tract)
-cohort.by.tract$tract = paste0('42', cohort.by.tract$tract)
+ellaBirths.SW = ellaAllBirths[ellaAllBirths$County %in% countymap$county, ]  ##ok.
+ellaBirths.SW$countynum = countymap$COUNTYFP[match(ellaBirths.SW$County,countymap$county)]
+head(ellaBirths.SW)
+table(ellaBirths.SW$County, ellaBirths.SW$countynum)
+table(ellaBirths.SW$County)
 
-### compare with census?
-census.allegheny = read.csv('ACSST5Y2020.S0101_2026-01-24T130140/ACSST5Y2020.S0101-Data.csv', header=T, skip = 1)
-head(census.allegheny) [1:6] #ok
+which(ellaBirths.SW$Tract=='')  #none!
+
+install.packages('xlsx')  #This package depends on Java and the rJava package
+ellaIQ = readxl::read_excel(path = 'IQ Loss in the 2019 Pittsburgh MSA Birth Cohort.xlsx', sheet=1, skip =2, col_names = T)
+
+countyBirthComparison = cbind(table(ellaBirths.SW$County), ellaIQ$`Live Births`[1:8])
+names(countyBirthComparison) =
+  c('ellaBirths.SW','ellaIQ')
+#' Armstrong and Fayette and Westmoreland are way off???
+
+
+### compare ellaBirths.SW with census?
+census.allegheny = read.csv('Census Data ACSST5Y2020.S0101_2026-01-24T130140/ACSST5Y2020.S0101-Data.csv', header=T, skip = 1)
 census.allegheny = data.frame(
   pop = census.allegheny$Estimate..Total..Total.population,
   tract = census.allegheny$Geography,
   lt5 = census.allegheny$Estimate..Total..Total.population..AGE..Under.5.years)
+head(census.allegheny) [1:3] #ok
 census.allegheny$tract = substr(start = 10, stop = 100, census.allegheny$tract)
+head(census.allegheny) [1:3] #ok
 
 census.allegheny$birth = census.allegheny$lt5/5
 dim(census.allegheny)   # only 394 tracts.  that's just allegheny.  so ok.
 head(census.allegheny)  # tract has '42'
 
 source('appPA/setcompare.R')
-setcompare(cohort.by.tract$tract, twt$tracts)
-# missing in 16.  Zeros?   723 overlapping tracts
-birthmatch = census.allegheny
-birthmatch$cohort = cohort.by.tract$births[
-  match(birthmatch$tract,  cohort.by.tract$tract)]
-sum(is.na(birthmatch$cohort))  #43.
+head(ellaBirths.SW)
+ellaBirths.SW$tract = paste0('42', ellaBirths.SW$countynum, ellaBirths.SW$tract)
+head(ellaBirths.SW)
 
 
-cor(birthmatch$lt5, birthmatch$cohort, use = 'pair')  # 0.67
-plot(birthmatch$lt5/5, birthmatch$cohort, xlab='census <5 divided by 5',
-     ylab='birth cohort')
+setcompare(ellaBirths.SW$tract, census.allegheny$tract)
+# 351 tracts overlap
+# both x_not_y y_not_x
+# 351     492      43
+table(ellaBirths.SW$tract)
+ellaBirths.SW.counts = data.frame(ellaCounts = table(ellaBirths.SW$tract),
+                                  tract = names(table(ellaBirths.SW$tract)))
+head(ellaBirths.SW.counts)
+sum(ellaBirths.SW$tract == '42003030500') ### checks out.
+census.counts = data.frame(tract=census.allegheny$tract, oneFifth = census.allegheny$lt5/5)
+head(census.counts)
+birthsComparison = merge(ellaBirths.SW.counts, census.counts)
+dim(birthsComparison)   ### 351
+birthsComparison$county = substr(birthsComparison$tract, 3,5)
+head(birthsComparison)
+
+cor(birthsComparison$ellaCounts.Freq, birthsComparison$oneFifth, use = 'pair')  # 0.67
+plot(birthsComparison$oneFifth, birthsComparison$ellaCounts.Freq,
+     xlab='census <5 divided by 5',
+     ylab='Ella birth cohort')
 title("Allegheny County")
 mtext('correlation = 0.67', side=3)
 abline(0,1)
 
-twt$births = cohort.by.tract$births[match(twt$tracts, cohort.by.tract$tract)]
-sum(is.na(twt$births))  #16    We will assume they are zeros.
+###   We will use
+twt$births = ellaBirths.SW.counts$ellaCounts.Freq[
+  match(twt$tracts, ellaBirths.SW.counts$tract)]
+sum(is.na(twt$births)) #   16 are missingm we presume no births.
 twt$births [is.na(twt$births)]  = 0
 
 cohort.births  = twt$births
--- appPA/countymap.R
+++ appPA/countymap.R
@@ -1,9 +1,15 @@
 ### county map
 table(PAtown$COUNTYFP)
 
+'https://en.wikipedia.org/wiki/List_of_counties_in_Pennsylvania'
+
+
 # 003 005 007 019 051 073 125 129
 # 402  19  51  44  36  28  59 100
 'Allegheny Armstrong Beaver Butler Fayette Lawrence Washington Westmoreland'
+#' confirmed at https://en.wikipedia.org/wiki/List_of_counties_in_Pennsylvania
+#' Also at https://unicede.air-worldwide.com/unicede/unicede_pennsylvania_fips.html
+#'   (county codes are different!)
 countymap = data.frame(COUNTYFP =
                          strsplit(split=' ', '003 005 007 019 051 073 125 129 ')[[1]],
                        county=
