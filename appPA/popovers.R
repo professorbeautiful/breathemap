1a79bc365cdb9052180b72bd04ab29c54354dd81
Roger Day <notsofeo@gmail.com>
2026-02-03 15:38:01 -0500

    The width of the modaldialog on the left is confused by the cute triangle notch they give you. So I write a bunch of hyphens to make it wide enough.

--- appPA/popovers.R
+++ appPA/popovers.R
@@ -9,39 +9,67 @@ addPopover(session = session,
            placement = 'left',
            id='featurePlot',
            title = 'About this graph:',
-           content=div(
+           content=div( style= 'width: 120% !important;',
+             "NOTE: ",
+             br(),
+             "The values plotted are for all ", strong("TRACTS"), "not communities.",
+             br(),
+             br(),
+             indentMe(
              "The toggle '...total'/'...rate per 1000'  ",
              br(),
              "is in effect for the nine ", strong("'harm'"),
              "features at the top.",
              br(),
-             "but irrelevant for the three ", strong("'information'"),
-             "features along the bottom.)",
+             "but irrelevant for the three ",
+             br(),
+             strong("'information'"),
+             "features along the bottom.)"
+             ),
              h5("If '...total' is selected:"),
              indentMe(
                "Yearly EXCESS harm estimates are totalled ",
                br(),
-               "for the selected tract or community."
+               "for the selected tract or community.",
+               br(),
+               "If 'communities/tracts' is set to 'communities',",
+               br(),
+               "this total is the sum",
+               br(),
+               "across the communities currently selected."
              ),
              h5("If '...rate per 1000' is selected:"),
              indentMe(
-               "Yearly estimates are rescaled to per 1000 people."
+               "Yearly estimates are rescaled to per 1000 people.",
+               br(),
+               "If 'communities/tracts' is set to 'communities',",
+               br(),
+               "this is the rate combined ",
+               br(),
+               "across the communities currently selected."
+
              ),
              h6("Special KEYS:"),
              indentMe(
-               "Type capital ", strong('H'), " to toggle between histogram and density plot.",
+               "Type capital ", strong('H'), " to toggle between ",
+               br(), "histogram and density plot.",
                br(),
-               "Type capital ", strong('O'), " for a popup to adjust the outlier quantile:",
+               "Type capital ", strong('O'), " for a popover to adjust the outlier quantile:",
                br(),
                indentMe(
-                 "Outliers not on the graph are listed on the right side.",
+                 "Outliers not on the graph ",
+                 br(),
+                 "are listed on the right side of the graph,",
+                 br(),
+                 " and in this  ", strong('O'), " quantile popover",
                  br(),
                  "To remove outlier handling, ",
                  br(),
                  "click in 'outlier quantile' box and delete."
-               )
-             )
 
+               )
+             ),
+             HTML(paste(rep('-', 50)) )
            ), trigger='hover'
 )
 
