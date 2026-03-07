8fcee96a45a7033254285573e390354d6c6e692b
Roger Day <notsofeo@gmail.com>
2026-03-07 10:19:18 -0500

    About this graph: removed section on  total and adjusted ( done elsewhere). And some indentation change.

--- appPA/popovers.R
+++ appPA/popovers.R
@@ -81,44 +81,12 @@ content_FeaturePlot_Information =
      "The values plotted are for all ", strong("TRACTS"), "not communities.",
      br(),
      br(),
-     indentMe(
-       "The toggle '...total'/'...adjusted for population'  ",
-       br(),
-       "is in effect for the nine ", strong("'harm'"),
-       "features at the top.",
-       br(),
-       "but irrelevant for the three ",
-       br(),
-       strong("'information'"),
-       "features along the bottom.)"
-     ),
-     h5("If '...total' is selected:"),
-     indentMe(
-       "Yearly EXCESS harm estimates are totalled ",
-       br(),
-       "for the selected tract or community.",
-       br(),
-       "If 'communities/tracts' is set to 'communities',",
-       br(),
-       "this total is the sum",
-       br(),
-       "across the communities currently selected."
-     ),
-     h5("If '...adjusted for population' is selected:"),
-     indentMe(
-       "yearly estimates are rescaled to per 1000 people.",
-       br(),
-       "If 'communities/tracts' is set to 'communities',",
-       br(),
-       "the rescaling is done combining  ",
-       br(),
-       "across the communities currently selected."
 
-     ),
      h6("Outlier handling:"),
      indentMe(
-       indentMe(
-         "Outliers not on the graph ",
+
+         "When '...total' is selected and 'communities' is selected,
+         Outliers not on the graph ",
          br(),
          "are listed on the right side of the graph,",
          br(),
@@ -127,21 +95,21 @@ content_FeaturePlot_Information =
          "To remove outlier handling, ",
          br(),
          "click in 'outlier quantile' box and delete."
-       ),
-       h6("Special KEYS for the outlier handling:"),
-       indentMe(
-         "Type  ", strong('9'), " for a popover to open the outlier quantile dialog",
-         br(),  ' while setting the outlier quantile to 0.999. ',
-         br(),
-         "Type capital ", strong('O'), " for a popover to open the outlier quantile dialog",
-         br(),  ' while setting the outlier quantile (no  outlier handling). ',
-         br()
+     ),
+     h6("Special KEYS for the outlier handling:"),
+     indentMe(
+       "Type  ", strong('9'), " for a popover to open the outlier quantile dialog",
+       br(),  ' while setting the outlier quantile to 0.999. ',
+       br(),
+       "Type capital ", strong('O'), " for a popover to open the outlier quantile dialog",
+       br(),  ' while setting the outlier quantile (no  outlier handling). ',
+       br()
 
-       ),
+       ,
        "Type capital ", strong('H'), " to toggle between ",
        "histogram and density plot.",
-     ),
-     HTML(paste(rep('-', 50)) )
+     )
+     #  ,      HTML(paste(rep('-', 50)) )  NEEDED only if popover on the left.
 )
 
 content_TotalOrRates_Information = HTML(paste(collapse=' ',
