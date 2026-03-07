84db3d1406ce629985c298cd65c8895a07cca65c
Roger Day <notsofeo@gmail.com>
2026-03-07 12:32:44 -0500

    Better text for content_FeaturePlot_Information

--- appPA/www/content_FeaturePlot_Information.R
+++ appPA/www/content_FeaturePlot_Information.R
@@ -1,13 +1,35 @@
 source('indentMe.R')
 content_FeaturePlot_Information =
   div( style= 'width: 120% !important;',
-       "NOTE: ",
+h5('This graph:'),
+       "The histogram values plotted are for all ", strong("TRACTS"), "not communities.",
        br(),
-       "The values plotted are for all ", strong("TRACTS"), "not communities.",
+       "(Type capital ", strong('H'), " to toggle between ",
+       "histogram and density plot.)",
        br(),
-       br(),
+       "A vertical arrow shows the value of the feature for the selected tract or community.",
 
-       h6("Outlier handling:"),
+       br(),
+       br(),
+       h5('Multiple tracts:'),
+      "When 'communities' is selected, there may be multiple tracts for the community.",
+      br(),
+      "If so, the  value for each tract is shown as a half-height green line.",
+      br(),
+      br(),
+      "If '...total' is selected, then the tract results are combined by adding.
+      <br>
+In that case, the total is written along the top of the graph,
+<br>
+with the number of tracts contributing.
+<br>
+The total might be larger than the right hand side of the feature axis.
+<br>
+This is indicated by a horizontal green arrow reaching to the right hand side.
+",
+      br(),      br(),
+
+       h5("Outlier handling:"),
        indentMe(
 
          "When '...total' is selected and 'communities' is selected,
@@ -21,7 +43,7 @@ content_FeaturePlot_Information =
          br(),
          "click in 'outlier quantile' box and delete."
        ),
-       h6("Special KEYS for the outlier handling:"),
+       h5("Special KEYS for the outlier handling:"),
        indentMe(
          "Type  ", strong('9'), " for a popover to open the outlier quantile dialog",
          br(),  ' while setting the outlier quantile to 0.999. ',
@@ -30,9 +52,6 @@ content_FeaturePlot_Information =
          br(),  ' while setting the outlier quantile (no  outlier handling). ',
          br()
 
-         ,
-         "Type capital ", strong('H'), " to toggle between ",
-         "histogram and density plot.",
        )
        #  ,      HTML(paste(rep('-', 50)) )  NEEDED only if popover on the left.
   )
